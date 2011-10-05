4a89fd0f75b5193149eab18e18cbe4fa28b1759e
Seth Ladd <sethladd@sethladd-macbookpro.local>
2011-10-05 09:34:41 -1000

    remove debug

--- src/site/_plugins/nav_link_tag.rb
+++ src/site/_plugins/nav_link_tag.rb
@@ -9,7 +9,6 @@ module Jekyll
 
     def render(context)
       page_url = context.environments.first["page"]["url"]
-      puts "XXXX #{page_url}"
       "<li>" +
       ((@url != page_url) ? "<a href=\"#{@url}\">" : '') +
       super.join('') +
