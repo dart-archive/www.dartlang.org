b31bbbab97d2f6fd20900023bbe98797e0b8f090
Emily Fortuna <efortuna@google.com>
2011-10-07 13:26:16 -0700

    fixed layout for libraries

--- src/site/docs/styleDocs.py
+++ src/site/docs/styleDocs.py
@@ -2,10 +2,38 @@
 
 # This is a simple script to convert the generated html api docs so that
 # they have a consistent look and feel with the rest of the dart website.
-# To run, type "python styleDocs.py" from this directory.
+# To run, type "python styleDocs.py" from this directory, and have the files
+# that need to be converted in the api directory.
 
 import os
 
+
+# Fix up the navigation page.
+f = open('api/index.html') 
+navFile = open('../_includes/documentationNav.html', 'w')
+# cycle past the initial html mumbo jumbo to get the content.
+line = f.readline()
+while (not ('<header>' in line)) and (line != ''):
+  line = f.readline()
+if (line != ''):
+  line = f.readline()
+  navFile.write('<section id="classes-nav">')
+  while not ('<footer>' in line)  and (line != ''):
+    #if '<ul>' in line: 
+    #  index = line.find('#')
+    #  while index != -1:
+    #    otherIndex = line.find('\'', index + 1)
+    #    line = line[:index] + line[otherIndex:]
+    #    index = line.find('#')
+    navFile.write(line)
+    line = f.readline()
+  navFile.write('</section>')
+    
+  f.close()
+  navFile.close()
+
+
+#Now fix all the other files.
 files = os.listdir("api")
 
 for file in files:
@@ -15,19 +43,32 @@ for file in files:
     # cycle past the initial html mumbo jumbo to get the content.
     line = f.readline()
     page_title = ''
-    while (not ('<header>' in line)) or (line == ''):
+    while (not ('<header>' in line)) and (line != ''):
       if '<title>' in line:
         page_title = line[len('<title>'): len(line) - len('</title>')- 1]
       line = f.readline()
     if (line != ''):
-      tempfile.write('''---
+      if file != 'index.html':
+        tempfile.write('''---
+layout: docsLayout
+title: "''' + page_title + '''" 
+---
+''')
+      else:
+        tempfile.write('''---
 layout: default
 title: "''' + page_title + '''" 
 ---
 ''')
-  
+
+
       line = f.readline()
-      while not ('</body>' in line ): 
+      while not ('<footer>' in line ) and (line != ''): 
+        if '<a name' in line: 
+          index = line.find('<a name')
+          otherIndex = line.find('>', index + 1)
+          line = line[:index] + line[otherIndex + 1:]
+          line.replace('''</a>''', '', 1)
         tempfile.write(line)
         line = f.readline()
 
@@ -36,4 +77,3 @@ title: "''' + page_title + '''"
 
       os.popen('rm api/' + file)
       os.popen('mv ' + 'tmpfile.foo api/' + file)
-    
