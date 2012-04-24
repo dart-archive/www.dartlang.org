c15e6ce23b296216920dad7cb5e762b4ed0c8ea6
Seth Ladd <sethladd@google.com>
2012-04-23 17:01:21 -0700

    track editor downloads
    BUG=
    TEST=
    
    Review URL: https://chromereviews.googleplex.com/4355012

---
+++ src/site/js/editor-downloads-analytics.js
@@ -0,0 +1,21 @@
+function main() {
+  var links = document.querySelectorAll('a.download-link');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+
+    link.addEventListener('click', function(e) {
+      var target = e.target;
+      var os = target.getAttribute('data-os') || 'unknown';
+      var bits = target.getAttribute('data-bits') || 'unknown';
+      var build = target.getAttribute('data-build') || 'unknown';
+      var bitsAndBuild = build + '/' + bits;
+      _gaq.push(['_trackEvent', 'Editor Downloads', os, bitsAndBuild]);
+    });
+  }
+}
+
+if (document.readyState != 'loading') {
+  main();
+} else {
+  document.addEventListener('DOMContentLoaded', main);
+}
