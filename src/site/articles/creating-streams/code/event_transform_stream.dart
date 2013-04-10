fc23b0eb7b5af80457a1479e843eb3949801e04e
Kathy Walrath <kathyw@google.com>
2013-04-10 10:04:00 -0700

    first draft Lasse's article
    Staged at:
    http://streams.dart-lang.appspot.com/articles/creating-streams/
    
    Also see:
    http://streams.dart-lang.appspot.com/authors/lasse-nielsen.html
    
    Review URL: https://chromiumcodereview.appspot.com/12780003

---
+++ src/site/articles/creating-streams/code/event_transform_stream.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+Stream<int> timedCounter(Duration interval, [int maxCount]) {
+  StreamController<int> controller;
+  Timer timer;
+  int counter = 0;
+  
+  void tick(_) {
+    counter++;
+    controller.add(counter); // Ask stream to send counter values as event.
+    if (maxCount != null && counter >= maxCount) {
+      timer.cancel();
+      controller.close();    // Ask stream to shut down and tell listeners.
+    }
+  }
+  
+  // Respond to pause or subscription state change.
+  void updatePauseState() {
+    if (controller.isPaused) {
+      // Stop the timer while paused.
+      if (timer != null) {
+        timer.cancel();
+        timer = null;
+      }
+    } else if (timer == null) {
+      // Restart timer.
+      timer = new Timer.periodic(interval, tick);
+    }
+  }
+  controller = new StreamController<int>(
+      onPauseStateChange: updatePauseState,
+      onSubscriptionStateChange: updatePauseState);
+  return controller.stream;
+}
+
+class MyMultiplyingStream extends EventTransformStream<int, int> {
+  // Create a new stream that repeats all events of [source] [times] times.
+  MyMultiplyingStream(Stream<int> source, int times)
+      : super(source, new MultiplyingEventTransformer(times));
+
+  // And the reason you needed to extend Stream.
+  num someMethodOnMyStream() => 42;
+}
+
+class MultiplyingEventTransformer extends StreamEventTransformer<int, int> {
+  final int times;
+  MultiplyingEventTransformer(this.times);
+
+  void handleData(int data, EventSink<int> sink) {
+    for (int i = 0; i < times; i++) sink.add(data);
+  }
+}
+
+main() {
+  Stream<int> stream = 
+      new MyMultiplyingStream(timedCounter(const Duration(seconds: 1), 15), 2);
+  stream.listen(print);  // Prints 1, 1, 2, 2, ..., 15, 15.
+}
---
+++ src/site/articles/creating-streams/code/transformer.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+/** Combines strings, breaking them at 80 characters. */
+class BlockBreaker extends StreamEventTransformer<String, String> {
+  String carry = '';
+  void handleData(String data, EventSink<String> output) {
+    data = carry + data;
+    data = data.replaceAll(new RegExp('\n'), ' '); // Remove newlines.
+    while (data.length >= 80) {
+      output.add(data.substring(0, 80));
+      data = data.substring(80);
+    }
+    carry = data;
+  }
+  void handleError(AsyncError error, EventSink<String> output) {
+    output.addError(error);
+  }
+  void handleDone(EventSink<String> output) {
+    if (carry.length > 0) {
+      output.add(carry);
+    }
+    output.close();
+  }
+}
+
+main() {
+  var part1 = '''
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+ipsam voluptatem quia voluptas sit ''';
+  var part2 = '''
+aspernatur aut odit aut fugit, sed quia
+consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+adipisci velit, sed quia non numquam eius modi tempora ''';
+  var part3 = '''
+incidunt ut labore et
+dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
+nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
+ex ea commodi consequatur?
+''';
+
+  var text = new StreamController<String>();
+  Stream lines = text.stream.transform(new BlockBreaker());
+  var lineCount = 0;
+  var sub;
+  sub = lines.listen((String line) {
+    lineCount++;
+    print('$lineCount: $line');
+  }, onDone: () {
+    print('Lines received: $lineCount');
+  });
+  text.add(part1);
+  text.add(part2);
+  text.add(part3);
+  text.close();
+}
