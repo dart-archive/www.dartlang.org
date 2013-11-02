52ae82c4707aa7542afb16aa13da501b4a12874f
Adam Singer <financeCoding@gmail.com>
2013-11-01 22:12:16 -0700

    updating javascript code

--- src/site/js/display-news.js
+++ src/site/js/display-news.js
@@ -4,17 +4,18 @@
     var feed = new google.feeds.Feed("http://news.dartlang.org/feeds/posts/default");
     feed.load(function(result) {
       if (!result.error) {
-        var ul = document.createElement("ul");
         for (var i = 0; i < result.feed.entries.length; i++) {
           var entry = result.feed.entries[i];
-          var li = document.createElement("li");
+          // var li = document.createElement("li");
           var a = document.createElement("a");
           a.setAttribute('href', entry.link);
           a.appendChild(document.createTextNode(entry.title));
-          li.appendChild(a);
-          ul.appendChild(li);
+          var p = document.createElement("p");
+          p.appendChild(document.createTextNode("news.dartlang.org"));
+          p.innerHTML+="&nbsp;&nbsp;&nbsp;";
+          p.appendChild(a);
+          container.appendChild(p);
         }
-        container.appendChild(ul);
       }
     });
   }
