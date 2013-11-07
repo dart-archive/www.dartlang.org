ea3cbd8a6ed4a407d8da52f4244d18cab2a9a803
Alan Joyce <alan@thisisalan.com>
2013-11-06 16:35:14 -0800

    clarifying variable name

--- src/site/js/display-news.js
+++ src/site/js/display-news.js
@@ -2,10 +2,10 @@
   function loadAndDisplayPosts() {
     var container = document.getElementById("feed-posts");
     var feed = new google.feeds.Feed("http://news.dartlang.org/feeds/posts/default");
-		var NUM_POSTS = 1;
+		var MAX_POSTS = 1;
     feed.load(function(result) {
       if (!result.error) {
-        for (var i = 0; i < result.feed.entries.length && i < NUM_POSTS; i++) {
+        for (var i = 0; i < result.feed.entries.length && i < MAX_POSTS; i++) {
           var entry = result.feed.entries[i];
           // var li = document.createElement("li");
           var a = document.createElement("a");
