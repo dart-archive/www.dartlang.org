7b8e0b3fd74dc184d978947b015b9eae5073b755
shaza <shaza@google.com>
2014-02-18 13:15:38 -0800

    Fixing a "closure in a loop" JavaScript problem.
    BUG=
    R=shailentuli@google.com
    
    Review URL: https://codereview.appspot.com/65500044

--- src/site/js/downloads-analytics.js
+++ src/site/js/downloads-analytics.js
@@ -6,25 +6,31 @@ function countClicks() {
   var links = document.querySelectorAll('a.download-link');
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
+    countClicksCallback(link)();
+  }
+}
+
+function countClicksCallback(link, target) {
+  return function() {
+    var downloadType;
 
     link.addEventListener('click', function(e) {
-      var downloadType;
       if (link.dataset['tool'] == "editor") {
         downloadType = "Editor Downloads";
       } else if (link.dataset['tool'] == "sdk") {
-        downloadType = "SDK Downloads";
+        downloadType =  "SDK Downloads";
       } else if (link.dataset['tool'] == "dartium") {
-        downloadType = "Dartium Downloads";
+        downloadType =  "Dartium Downloads";
       } else {
-        downloadType = "Unknown Downloads";
+        downloadType =  "Unknown Downloads";
       }
 
-      var target = e.currentTarget;
+      target = e.currentTarget;
       var os = target.getAttribute('data-os') || 'unknown';
       var bits = target.getAttribute('data-bits') || 'unknown';
       var build = target.getAttribute('data-build') || 'unknown';
       var bitsAndBuild = build + '/' + bits;
       _gaq.push(['_trackEvent', downloadType, os, bitsAndBuild, 1]);
     });
-  }
+  };
 }
