0f085c6c1c31f0921f61d18d13913170aac1d276
Seth Ladd <sethladd@google.com>
2014-04-03 16:15:03 -0700

    new I/O perf page
    BUG=
    R=kathyw@google.com
    
    Review URL: https://chromiumcodereview.appspot.com//224143004

--- src/site/js/performance-charts.js
+++ src/site/js/performance-charts.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
-  var nav = $('#performance-charts-nav');
-  var charts = $('#performance-charts');
+  function init(name) {
+    var nav = $('#performance-charts-nav-' + name);
+    var charts = $('#performance-charts-' + name);
 
-  $.getJSON('//public-golem.storage.googleapis.com/benchmarks.json',
-    function(data) {
-      $.each(data, function(index) {
-        var benchmark = data[index];
-        $(nav).append('<li class="' +
-          (index == 0 ? 'active' : '') +
-          '"><a href="#' + benchmark['name'] +
-          '" data-toggle="tab">' + benchmark['name'] + '</a></li>');
-        $(charts).append('<div class="tab-pane ' +
-          (index == 0 ? 'active' : '') +
-          '" id="' + benchmark['name'] + '">' +
-          '<iframe style="border: 0" src="//public-golem.storage.googleapis.com/' +
-          benchmark['graph'] + '" ' +
-          'width="900" height="440"></iframe></div>');
+    $.getJSON('//public-golem.storage.googleapis.com/' + name + '_benchmarks.json',
+      function(data) {
+        $.each(data, function(index) {
+          var benchmark = data[index];
+          var linkName = benchmark['name'].replace(/ /g, '');
+          $(nav).append('<li class="' +
+            (index == 0 ? 'active' : '') +
+            '"><a href="#' + linkName +
+            '" data-toggle="tab">' + benchmark['name'] + '</a></li>');
+          $(charts).append('<div class="tab-pane ' +
+            (index == 0 ? 'active' : '') +
+            '" id="' + linkName + '">' +
+            '<iframe style="border: 0" src="//public-golem.storage.googleapis.com/' +
+            benchmark['graph'] + '" ' +
+            'width="900" height="440"></iframe></div>');
+        });
       });
-    });
+  }
+  init("base");
+  init("io");
 });
